19af9335c8657496bb913a93964a81c2e7763bb2
alisonsantosofc <alisonsantoso22@gmail.com>
2022-03-10 03:10:17 -0300

    Create global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,1 +1,38 @@
 import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  :root {
+    --orange-500: #FF5733;
+
+    --bg-body: #12283C;
+
+    --boxshadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.15);
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  body {
+    background: var(--bg-body);
+    color: #fff;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, button, input {
+    font-family: 'Poppins', sans-serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
